715b434f78491ce23e4a3cd680667c5a37775e2c
Aditya Mehto <101061456+aditya9575@users.noreply.github.com>
2023-10-25 13:12:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,3 @@
-// import { useState } from 'react'
-// import './App.css'
-// import Header from './Header'
-// import Sidebar from './Sidebar'
-// import Home from './Home'
-
-// function App() {
- 
-
-//   return (
-//     <div className='grid-container'>
-//      <Header />
-//      <Sidebar/>
-//      <Home/>
-//     </div>
-//   )
-// }
-
-// export default App
-
-
 import { useState } from 'react'
 import './App.css'
 import Header from './Header'
