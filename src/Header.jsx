d4b82c0573d20d9e365d36e267baebc24ccc95ee
Aditya Mehto <101061456+aditya9575@users.noreply.github.com>
2023-10-25 13:13:24 +0530

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,33 +1,3 @@
-// import React from 'react'
-// import {BsFillBellFill , BsFillEnvelopeFill , BsPersonCircle , BsSearch , BsJustify} from 'react-icons/bs'
-
-
-// function Header() {
-//   return (
-//     <header className='header'>
-
-// <div className='menu-icon'>
-// {/* to show our sidebar on smaller devices we use this  BsJustify */}
-//  <BsJustify className='icon'/>
-// </div>
-
-// <div className='header-left'>
-//   <BsSearch className='icon'/>
-// </div>
-
-// <div className='header-right'>
-//  <BsFillBellFill className='icon'/>
-//  <BsFillEnvelopeFill className='icon'/>
-//  <BsPersonCircle className='icon'/>
-// </div>
-
-//     </header>
-//   )
-// }
-
-// export default Header
-
-
 import React from 'react'
 import 
  {BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify}
